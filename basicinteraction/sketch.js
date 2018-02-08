7d60ab4cc2391f94627bb3a64c54b6f11593fb65
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-08 15:03:05 -0600

    shapes
    things that make shapes

---
+++ basicinteraction/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  noCursor();
+}
+function draw(){
+}
+function mouseMoved(){
+  background(255,255,255);
+  fill(random(0,255),random(0,255),random(0,255));
+  noStroke();
+  ellipse(mouseX,mouseY,random(0,100),random(0,100));
+}
+function deviceShaken(){
+    fill(random(0,255),random(0,255),random(0,255));
+    noStroke();
+    ellipse(random(0,100),random(0,100),random(0,100),random(0,100));
+}
