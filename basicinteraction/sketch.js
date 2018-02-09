0e4b0c8baf123d46054fc13f559ac205939e9732
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-09 14:36:12 -0600

    change
    change #2

--- basicinteraction/sketch.js
+++ basicinteraction/sketch.js
@@ -1,6 +1,6 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  noCursor();
+    noCursor();
 }
 function draw(){
 }
@@ -11,7 +11,7 @@ function mouseMoved(){
   ellipse(mouseX,mouseY,random(0,100),random(0,100));
 }
 function deviceShaken(){
-    fill(random(0,255),random(0,255),random(0,255));
-    noStroke();
-    ellipse(random(),random(),random(),random());
+  fill(random(0,255),random(0,255),random(0,255));
+  noStroke();
+  ellipse(random(),random(),random(),random());
 }
