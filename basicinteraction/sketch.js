ba9be09163fb48334c16cc8f437bfb286e2759e9
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-09 15:13:19 -0600

    change
    clothes

--- basicinteraction/sketch.js
+++ basicinteraction/sketch.js
@@ -1,17 +1,13 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
-    noCursor();
-}
-function draw(){
-}
+    noCursor();}
+function draw(){}
 function mouseMoved(){
   background(255,255,255);
   fill(random(0,255),random(0,255),random(0,255));
   noStroke();
-  ellipse(mouseX,mouseY,random(0,100),random(0,100));
-}
+  ellipse(mouseX,mouseY,random(0,100),random(0,100));}
 function deviceShaken(){
   fill(random(0,255),random(0,255),random(0,255));
   noStroke();
-  ellipse(random(),random(),random(),random());
-}
+  ellipse(random(),random(),random(),random());}
