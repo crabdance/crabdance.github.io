7c0f37605fd53841cfa7fcacdb95a278450ad545
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-12 14:52:29 -0600

    shapes
    makes shapes from screen function

--- basicinteraction/sketch.js
+++ basicinteraction/sketch.js
@@ -1,13 +1,13 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
-    noCursor();}
+  noCursor();}
 function draw(){}
 function mouseMoved(){
   background(255,255,255);
   fill(random(0,255),random(0,255),random(0,255));
   noStroke();
   ellipse(mouseX,mouseY,random(0,100),random(0,100));}
-function deviceShaken(){
+function deviceShaken() {
   fill(random(0,255),random(0,255),random(0,255));
   noStroke();
   ellipse(random(),random(),random(),random());}
---
+++ screenshapes/p5/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  if (mouseX = width/2); {
+    ellipse(random(),random(),random(0,100),random(0,100));
+  }
+}
