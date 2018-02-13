8a69ae38bca24846e846cd56939341e9e4d96339
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-13 15:01:07 -0600

    change
    change shirt

--- basicinteraction/sketch.js
+++ basicinteraction/sketch.js
@@ -1,13 +1,25 @@
-function setup(){
-  createCanvas(windowWidth,windowHeight);
-  noCursor();}
-function draw(){}
-function mouseMoved(){
-  background(255,255,255);
-  fill(random(0,255),random(0,255),random(0,255));
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+}
+
+function draw() {}
+
+function mouseMoved() {
+  background(255, 255, 255);
+  fill(random(0, 255), random(0, 255), random(0, 255));
   noStroke();
-  ellipse(mouseX,mouseY,random(0,100),random(0,100));}
+  ellipse(mouseX, mouseY, random(0, 100), random(0, 100));
+}
+
+function mousePressed(){
+  background(255, 255, 255);
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  
+}
+
 function deviceShaken() {
-  fill(random(0,255),random(0,255),random(0,255));
+  fill(random(0, 255), random(0, 255), random(0, 255));
   noStroke();
-  ellipse(random(),random(),random(),random());}
+  ellipse(random(), random(), random(), random());
+}
