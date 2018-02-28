b0a0aa675ad9b1af60c0fd5e184ef6e417fabe5c
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-28 15:00:06 -0600

    lights
    actually made traffic lights work

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -6,13 +6,72 @@
 // changing according to time. You may want to investigate the millis()
 // function at processing.org/reference.
 
+let state;
+let redLightDuration, yellowLightDuration, greenLightDuration;
+let lastTimeLightChanged;
+
 function setup() {
   createCanvas(600, 600);
+  state = 1;
+  redLightDuration = 3000;
+  yellowLightDuration = 500;
+  greenLightDuration = 3000;
+  lastTimeLightChanged = millis();
 }
 
 function draw() {
   background(255);
   drawOutlineOfLights();
+  checkIfLightSwitched();
+  displayCorrectLight();
+}
+
+function checkIfLightSwitched(){
+  if (state === 1){
+    if (millis() > lastTimeLightChanged + greenLightDuration){
+      state = 2;
+      lastTimeLightChanged = millis();
+    }
+  }
+  if (state === 2){
+    if (millis() > lastTimeLightChanged + yellowLightDuration){
+      state = 3;
+      lastTimeLightChanged = millis();
+    }
+  }
+  if (state === 3){
+    if (millis() > lastTimeLightChanged + redLightDuration){
+      state = 1;
+      lastTimeLightChanged = millis();
+    }
+  }
+}
+
+function displayCorrectLight(){
+  if (state === 1){
+    drawGreenLight();
+  }
+  else if (state === 2){
+    drawYellowLight();
+  }
+  else if (state === 3){
+    drawRedLight();
+  }
+}
+
+function drawGreenLight(){
+  fill(0,255,0);
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function drawYellowLight(){
+  fill(255,255,0);
+  ellipse(width/2, height/2, 50, 50); //middle
+}
+
+function drawRedLight(){
+  fill(255,0,0);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
 }
 
 function drawOutlineOfLights() {
@@ -22,7 +81,7 @@ function drawOutlineOfLights() {
   rect(width/2, height/2, 75, 200, 10);
 
   //lights
-  fill(255);
+  fill(0);
   ellipse(width/2, height/2 - 65, 50, 50); //top
   ellipse(width/2, height/2, 50, 50); //middle
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
