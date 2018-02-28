33fcf86b782b0e19c34cd12b16910c206419f55f
crabdance <36053932+crabdance@users.noreply.github.com>
2018-02-28 15:03:54 -0600

    off
    ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooof

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -26,63 +26,61 @@ function draw() {
   displayCorrectLight();
 }
 
-function checkIfLightSwitched(){
-  if (state === 1){
-    if (millis() > lastTimeLightChanged + greenLightDuration){
+function checkIfLightSwitched() {
+  if (state === 1) {
+    if (millis() > lastTimeLightChanged + greenLightDuration) {
       state = 2;
       lastTimeLightChanged = millis();
     }
   }
-  if (state === 2){
-    if (millis() > lastTimeLightChanged + yellowLightDuration){
+  if (state === 2) {
+    if (millis() > lastTimeLightChanged + yellowLightDuration) {
       state = 3;
       lastTimeLightChanged = millis();
     }
   }
-  if (state === 3){
-    if (millis() > lastTimeLightChanged + redLightDuration){
+  if (state === 3) {
+    if (millis() > lastTimeLightChanged + redLightDuration) {
       state = 1;
       lastTimeLightChanged = millis();
     }
   }
 }
 
-function displayCorrectLight(){
-  if (state === 1){
+function displayCorrectLight() {
+  if (state === 1) {
     drawGreenLight();
-  }
-  else if (state === 2){
+  } else if (state === 2) {
     drawYellowLight();
-  }
-  else if (state === 3){
+  } else if (state === 3) {
     drawRedLight();
   }
 }
 
-function drawGreenLight(){
-  fill(0,255,0);
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+function drawGreenLight() {
+  fill(0, 255, 0);
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
 }
 
-function drawYellowLight(){
-  fill(255,255,0);
-  ellipse(width/2, height/2, 50, 50); //middle
+function drawYellowLight() {
+  fill(255, 255, 0);
+  ellipse(width / 2, height / 2, 50, 50); //middle
 }
 
-function drawRedLight(){
-  fill(255,0,0);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
+function drawRedLight() {
+  fill(255, 0, 0);
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
 }
 
 function drawOutlineOfLights() {
   //box
   rectMode(CENTER);
   fill(0);
-  rect(width/2, height/2, 75, 200, 10);
+  rect(width / 2, height / 2, 75, 200, 10);
 
   //lights
-  fill(0);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
-  ellipse(width/2, height/2, 50, 50); //middle
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  fill(255);
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
+  ellipse(width / 2, height / 2, 50, 50); //middle
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
 }
