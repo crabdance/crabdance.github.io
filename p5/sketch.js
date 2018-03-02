5ecf112437cf9cf3c9fb60c4a91410dc06ee1f5a
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-02 15:12:43 -0600

    oof
    ooooooooooooooooooooooooooooooooooooooooooooof

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,7 +1,25 @@
-function setup() {
-  // put setup code here
+let boxSize;
+let isFilled;
+function setup(){
+  createCanvas(600,600);
+  boxSize=width/8;
+  isFilled=false;
 }
-
-function draw() {
-  // put drawing code here
+function draw(){
+  background(255);
+  drawBoard();
+}
+function drawBoard(){
+  for(let i=0;i<8;i++){
+    for(let j=0;j<8;j++){
+      if(isFilled){
+        fill(0);
+      }else{
+        fill(255);
+      }
+      rect(boxSize*j,boxSize*i,boxSize,boxSize);
+      isFilled=!isFilled;
+    }
+    isFilled=!isFilled;
+  }
 }
