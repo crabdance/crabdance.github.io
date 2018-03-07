6bc6a2eac0aaad353f20ccba5f51f84036dafc59
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-07 14:59:29 -0600

    oof
    oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooof

---
+++ stateassingment/p5/sketch.js
@@ -0,0 +1,33 @@
+let state = 0;
+let a;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+}
+
+function draw() {
+  background(255);
+  if (state === 0) {
+    controlGlobe();
+  }
+  if (state === 1) {
+    globeShift()
+  }
+}
+
+function controlGlobe() {
+  fill(255, 0, 0);
+  sphere(a);
+}
+
+function mousePressed() {
+  state = 1;
+}
+
+function globeShift() {
+  for (let a = 20; a < 300; a=a + 1){
+    sphere(a);
+  }
+}
