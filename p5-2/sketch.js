eb3055dea840446e4b3b323ff37e6cad6ff3da26
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-13 15:12:24 -0600

    fffff
    fc

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -5,10 +5,15 @@ function setup() {
 function draw() {
   background(255);
 
+  push();
   angleMode(DEGREES);
-  translate(100,100);
-  rotate(45);
+  translate(width/2,height/2);
+  rotate(frameCount * 20000);
 
   fill(0);
+  rectMode(CENTER);
   rect(0, 0, 300, 50);
+  pop();
+
+  rect(100,100,200,200);
 }
