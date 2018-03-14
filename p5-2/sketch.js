c55bfcde6178ff660676f7a931c29b60759bb590
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-14 15:21:29 -0600

    j

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -1,19 +1,41 @@
+let clockSize;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  if (width > height) {
+    clockSize = height * 0.9;
+  } else {
+    clockSize = width * 0.9;
+  }
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(255);
 
   push();
-  angleMode(DEGREES);
-  translate(width/2,height/2);
-  rotate(frameCount * 20000);
+  translate(width / 2, height / 2);
 
-  fill(0);
-  rectMode(CENTER);
-  rect(0, 0, 300, 50);
+  //clockHousing
+  fill(255);
+  strokeWeight(5);
+  ellipse(0, 0, clockSize, clockSize);
+
+  //hour tic-marks
+  strokeWeight(10);
+  strokeCap(SQUARE);
+  for (let i = 0; i < 12; i++) {
+    line(0, clockSize / 2 * 0.95, 0, clockSize / 2 * 0.8);
+    rotate(360 / 12);
+
+  }
+
+  strokeWeight(4);
+  strokeCap(SQUARE);
+  for (let i = 0; i < 60; i++) {
+    line(0, clockSize / 2 * 0.95, 0, clockSize / 2 * 0.80);
+    rotate(360 / 60);
+  }
+
   pop();
-
-  rect(100,100,200,200);
 }
