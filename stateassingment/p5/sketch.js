bd69b290f50a38e01b711481764641699979846f
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-15 15:09:59 -0600

    off

--- stateassingment/p5/sketch.js
+++ stateassingment/p5/sketch.js
@@ -8,12 +8,23 @@ function draw(){
   if (state === 0){
     createStartMenu();
   }
-
+  if (state === 1){
+    background(255,0,0);
+  }
 }
 
 function createStartMenu(){
+  fill (255,0,0);
   rect(width/2,height/2,20,20);
-  
+
+}
+
+function startSwitch(){
+  if mouseClicked(){
+    if (mouseX === width / 2 && mouseY ==== height / 2){
+      state = 1;
+    }
+  }
 }
 
 function createRandom(){
