bd906c961d41968657e4ce23fdfcf792b50a431c
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-27 14:54:51 -0600

    fuck

--- gameoflife/sketch.js
+++ gameoflife/sketch.js
@@ -23,16 +23,38 @@ function draw() {
 }
 
 function nextTurn() {
+
+  let next = createEmpty2dArray(cols, rows);
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[0].length; y++) {
       let neighbors = 0;
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
-          neighbors += grid[x + i][y + j];
+          // don't break on the edges
+          if (x + i >= 0 && x + i < cols && y + j >= 0 && y + j < cols) {
+            neighbors += grid[x + i][y + j];
+          }
+        }
+      }
+      neighbors -= grid[x][y];
+
+      //apply the rules of the game
+      if (grid[x][y] === 1) {  // currently alive
+        if (neighbors === 2 || neighbors === 3) {
+          next[x][y] = 1;
+        } else {
+          next [x][y] = 0;
+        }
+      } else {
+        if (neighbors === 3) {
+          next [x][y] = 1;
+        } else {
+          next [x][y] = 0;
         }
       }
     }
   }
+  grid = next;
 }
 
 function displayGrid() {
@@ -62,7 +84,7 @@ function mousePressed() {
 function keyPressed() {
   if (key === "r" || key === "R") {
     grid = createRandom2dArray(cols, rows);
-  } else if (key === " ") {
+  } else if (key === "a") {
     nextTurn();
   }
 
@@ -81,4 +103,17 @@ function createRandom2dArray(cols, rows) {
     }
   }
   return randomGrid;
+}
+
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
+  }
 }
