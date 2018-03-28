5bf15a52ab6e19c3b3c64c3507b4f45dc4f06ab1
crabdance <36053932+crabdance@users.noreply.github.com>
2018-03-28 15:14:16 -0600

    guck

--- gridassingment/sketch.js
+++ gridassingment/sketch.js
@@ -1,4 +1,7 @@
-
+let rows = 10;
+let cols = 10;
+let grid;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,5 +9,19 @@ function setup() {
 
 function draw() {
   background(255);
+  displayGrid();
 
+}
+
+function displayGrid() {
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[0].length; y++) {
+      if (grid[x][y] === 0) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
 }
